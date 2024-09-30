433745b4cc9c5a184daf6cc9352d7e2461687544
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-09-30 16:27:15 +0300

    Update AngryProfessor.py

--- AngryProfessor.py
+++ AngryProfessor.py
@@ -2,7 +2,6 @@
 # to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time 
 # (arrivalTime <= 0) to arrived late (arrivalTime > 0) Given the arrival time of each student and a threshhold number of 
 # attendees, determine if the class is cancelled. 
-
 import math
 import os
 import random
