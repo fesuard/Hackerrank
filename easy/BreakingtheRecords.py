200161abecabdaad4328959f4e800eefc22f36f1
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-09-30 17:30:07 +0300

    Update and rename Breaking the Records to easy/BreakingtheRecords.py

--- easy/BreakingtheRecords.py
+++ easy/BreakingtheRecords.py
@@ -1,8 +1,15 @@
-https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
-Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number 
-of times she breaks her season record for most points and least points in a game. Points scored in the first game establish 
-her record for the season, and she begins counting from there.
-Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+# https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+# Maria plays college basketball and wants to go pro. Each season she maintains a record of 
+# her play. She tabulates the number of times she breaks her season record for most points 
+# and least points in a game. Points scored in the first game establish her record for the
+# season, and she begins counting from there. Given the scores for a season, determine the 
+# number of times Maria breaks her records for most and least points scored during the season.
+import math
+import os
+import random
+import re
+import sys
+
 
 def breakingRecords(scores):
     temp_scores = [scores[0],scores[0]]
@@ -15,6 +22,7 @@ def breakingRecords(scores):
             temp_scores[1] = score
             scores_count[1] += 1
     return scores_count
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
