fde6ba56d9af7f95899ea84514c00cb5136213e8
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-09-30 16:36:57 +0300

    Update AppleandOrange.py

--- AppleandOrange.py
+++ AppleandOrange.py
@@ -24,3 +24,29 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         if d + b in range(s, t+1):
             oranges_count += 1
     print(f"{apples_count}\n{oranges_count}")
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
