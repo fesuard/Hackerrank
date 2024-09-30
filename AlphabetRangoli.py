737b8a16ed35c7bdc0ae1a2607088ffe166114ed
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-09-30 16:17:34 +0300

    added short problem description

--- AlphabetRangoli.py
+++ AlphabetRangoli.py
@@ -1,15 +1,20 @@
-https://www.hackerrank.com/challenges/alphabet-rangoli/problem
-You are given an integer, N. Your task is to print an alphabet rangoli of size N. 
-(Rangoli is a form of Indian folk art based on creation of patterns.)
-Different sizes of alphabet rangoli are shown below:
-#size 3
-----c----
---c-b-c--
-c-b-a-b-c
---c-b-c--
-----c----
-
+# https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+# You are given an integer, N. Your task is to print an alphabet rangoli of size N. 
+# (Rangoli is a form of Indian folk art based on creation of patterns.)
+# Different sizes of alphabet rangoli are shown below:
+# #size 3
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
 import string
+import math
+import os
+import random
+import re
+import sys
+
 
 def print_rangoli(size):
     letters = string.ascii_lowercase
