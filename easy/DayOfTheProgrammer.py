4b95e58488617b941e54efd799f9587d9cd16446
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-01 16:51:29 +0300

    Update and rename Day of the Programmer to easy/DayOfTheProgrammer.py

--- easy/DayOfTheProgrammer.py
+++ easy/DayOfTheProgrammer.py
@@ -1,8 +1,15 @@
-https://www.hackerrank.com/challenges/day-of-the-programmer/problem
-Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the 
-Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700. 
-Given a year y, find the date of the 256th day of that year according to the official Russian calendar during that year.
-Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is y.
+# https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+# Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the 
+# Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700. 
+# Given a year y, find the date of the 256th day of that year according to the official Russian calendar 
+# during that year.Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit
+# month, and yyyy is y.
+import math
+import os
+import random
+import re
+import sys
+
 
 def dayOfProgrammer(year):
     if year >= 1700 and year <= 1917:
@@ -17,6 +24,7 @@ def dayOfProgrammer(year):
             return f'12.09.{year}'
         else:
             return f'13.09.{year}'
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
