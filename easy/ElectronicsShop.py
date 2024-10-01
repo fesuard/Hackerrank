02fdb1a824f3d6bb0386b534b0386b68bd777aa7
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-01 18:24:09 +0300

    Update and rename Electronics Shop to easy/ElectronicsShop.py

--- easy/ElectronicsShop.py
+++ easy/ElectronicsShop.py
@@ -1,5 +1,13 @@
-A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. 
-Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
+# https://www.hackerrank.com/challenges/electronics-shop/problem
+# A person wants to determine the most expensive computer keyboard and USB drive that can be 
+# purchased with a give budget. Given price lists for keyboards and USB drives and a budget, 
+# find the cost to buy them. If it is not possible to buy both items, return -1.
+import math
+import os
+import random
+import re
+import sys
+
 
 def getMoneySpent(keyboards, drives, b):
     prices = []
@@ -14,12 +22,21 @@ def getMoneySpent(keyboards, drives, b):
              
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
     bnm = input().split()
+    
     b = int(bnm[0])
+    
     n = int(bnm[1])
+    
     m = int(bnm[2])
+    
     keyboards = list(map(int, input().rstrip().split()))
+    
     drives = list(map(int, input().rstrip().split()))
+    
     moneySpent = getMoneySpent(keyboards, drives, b)
+    
     fptr.write(str(moneySpent) + '\n')
+    
     fptr.close()
