7162993fa8bbf25d838abba416e78cc9eae10802
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-01 16:47:30 +0300

    Update and rename CutTheSticks.py to easy/CutTheSticks.py

--- easy/CutTheSticks.py
+++ easy/CutTheSticks.py
@@ -5,7 +5,6 @@
 # of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so 
 # discard them. Given the lengths of n sticks, print the number of sticks that are left before each iteration
 # until there are none left.
-
 import math
 import os
 import random
@@ -13,12 +12,26 @@ import re
 import sys
 
 
+# using count
 def cutTheSticks(arr):
     n = len(arr)
     srt = sorted(list(set(arr)))
     res = [n]
     for no in srt:
         n -= arr.count(no)
+        res.append(n)
+    return res[:-1]
+
+# hashmap
+def cutTheSticks1(arr):
+    hashm = {}
+    n = len(arr)
+    res = [n]
+    for elem in arr:
+        hashm[elem] = hashm.get(elem, 0) + 1
+    keys = sorted(hashm.keys())
+    for key in keys:
+        n -= hashm[key]
         res.append(n)
     return res[:-1]
 
