451a1a1a3426407d823fe810ad401c6f355c52d5
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-02 18:04:16 +0300

    Update and rename Find the Runner-Up Score! to easy/FindTheRunnerUpScore.py

--- easy/FindTheRunnerUpScore.py
+++ easy/FindTheRunnerUpScore.py
@@ -1,9 +1,12 @@
-https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
-Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score.
+# You are given n scores. Store them in a list and find the score of the runner-up.
+import math
+import os
+import random
+import re
+import sys
 
-Input Format
-
-The first line contains n. The second line contains an array A[] of n integers each separated by a space.
 
 #using sort():
 if __name__ == '__main__':
