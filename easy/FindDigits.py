de2aa945bb5b5ce82f37c7f30013df1d255bc636
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-02 19:33:07 +0300

    Update and rename FindDigits.py to easy/FindDigits.py

--- easy/FindDigits.py
+++ easy/FindDigits.py
@@ -1,7 +1,6 @@
 # https://www.hackerrank.com/challenges/find-digits/problem
 # An integer d is a divisor of an integer n if the remainder of n % d = 0. Given an integer, for each digit that makes up 
 # the integer determine whether it is a divisor. Count the number of divisors occurring within the integer. 
-
 import math
 import os
 import random
