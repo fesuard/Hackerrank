e9a3fede7a05448dde8ff3388d6090567b937fe2
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-03 13:31:00 +0300

    Update and rename Finding the percentage to easy/FindingThePercentage.py

--- easy/FindingThePercentage.py
+++ easy/FindingThePercentage.py
@@ -1,6 +1,7 @@
-https://www.hackerrank.com/challenges/finding-the-percentage/problem
-The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
-Print the average of the marks array for the student name provided, showing 2 places after the decimal. 
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a
+# list of students. Print the average of the marks array for the student name provided, showing 2
+# places after the decimal. 
 
 if __name__ == '__main__':
     n = int(input())
