f0e07df71c4fcec59add92f7b78a8841c5b715a5
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-03 18:30:35 +0300

    Update PickingNumbers.py

--- easy/PickingNumbers.py
+++ easy/PickingNumbers.py
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/picking-numbers/problem
-# Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1. 
+# Given an array of integers, find the longest subarray where the absolute difference between any two
+# elements is less than or equal to 1. 
 import math
 import os
 import random
