2fef1673886d594439da16d6bd9102290b5a3a21
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-04 19:17:02 +0300

    Update and rename SequenceEquation.py to easy/SequenceEquation.py

--- easy/SequenceEquation.py
+++ easy/SequenceEquation.py
@@ -1,8 +1,7 @@
 # https://www.hackerrank.com/challenges/permutation-equation/problem
-# Given a sequence of n integers, (p(1), p(2), p(3), p(n)) where each element is distinct and satisfies 1 <= p(x) <= n. 
-# For each x where 1 <= x <= n, that is x increments from 1 to n, find any integer y such that p(p(y)) == x and keep a 
-# history of the values of y in a return array.
-
+# Given a sequence of n integers, (p(1), p(2), p(3), p(n)) where each element is distinct and 
+# satisfies 1 <= p(x) <= n. For each x where 1 <= x <= n, that is x increments from 1 to n, find
+# any integer y such that p(p(y)) == x and keep a history of the values of y in a return array.
 import math
 import os
 import random
