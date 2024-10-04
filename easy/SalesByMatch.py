c5f0a6dfdbb0f53a5e0201113d2521a2c3633ee5
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-04 19:14:00 +0300

    Update and rename Sales by Match to easy/SalesByMatch.py

--- easy/SalesByMatch.py
+++ easy/SalesByMatch.py
@@ -1,11 +1,12 @@
-There is a large pile of socks that must be paired by color. Given an array of integers representing 
-the color of each sock, determine how many pairs of socks with matching colors there are.
-Complete the sockMerchant function in the editor below.
-sockMerchant has the following parameter(s):
-    int n: the number of socks in the pile
-    int ar[n]: the colors of each sock
-Returns
-    int: the number of pairs
+# https://www.hackerrank.com/challenges/sock-merchant/problem
+# There is a large pile of socks that must be paired by color. Given an array of integers representing 
+# the color of each sock, determine how many pairs of socks with matching colors there are.
+import math
+import os
+import random
+import re
+import sys
+
 
 def sockMerchant(n, ar):
     hmap = dict()
