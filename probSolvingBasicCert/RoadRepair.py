0a4156497062f6fee4d189a5256efce4f520a2bd
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-04 19:02:31 +0300

    Update and rename Road repair (from basic problem solving cert) to probSolvingBasicCert/RoadRepair.py

--- probSolvingBasicCert/RoadRepair.py
+++ probSolvingBasicCert/RoadRepair.py
@@ -1,11 +1,11 @@
 # Complete the 'getMinCost' function below.
-#
 # The function is expected to return a LONG_INTEGER.
 # The function accepts following parameters:
 #  1. INTEGER_ARRAY crew_id
 #  2. INTEGER_ARRAY job_id
 # len(crew_id) = len(job_id)
 # It needs to return the minimum distance for the crew to travel to the available jobs
+
 
 def getMinCost(crew_id, job_id):
     crew_id.sort()
