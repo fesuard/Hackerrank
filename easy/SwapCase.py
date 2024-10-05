fbe6e1f595d884c1d800809ff47c5c4a10464a09
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-05 13:31:52 +0300

    Update and rename sWAP cASE to easy/SwapCase.py

--- easy/SwapCase.py
+++ easy/SwapCase.py
@@ -1,14 +1,17 @@
-https://www.hackerrank.com/challenges/swap-case/problem
-You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+# https://www.hackerrank.com/challenges/swap-case/problem
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+
 def swap_case(s):
     res = ''
     for char in s:
-        if char == char.upper():
+        if char.isupper():
             char = char.lower()
         else:
             char = char.upper()
         res += char
     return res
+
 
 if __name__ == '__main__':
     s = input()
