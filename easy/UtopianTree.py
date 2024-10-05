fd441e4c0b2ec6a46a72228fbf3f1792b6399a0f
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-05 12:49:00 +0300

    Update and rename UtopianTree.py to easy/UtopianTree.py

--- easy/UtopianTree.py
+++ easy/UtopianTree.py
@@ -1,8 +1,7 @@
+# https://www.hackerrank.com/challenges/utopian-tree/problem
 # The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, 
 # its height increases by 1 meter. A Utopian Tree sapling with a height of 1 meter is planted at the onset of 
 # spring. How tall will the tree be after n growth cycles?
-
-
 import math
 import os
 import random
@@ -18,6 +17,7 @@ def utopianTree(n):
         else: 
             res += 1
     return res
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
