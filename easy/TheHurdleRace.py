6044ba31e86a2ef74ec1574d3114eacfd2eabca5
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-05 12:36:14 +0300

    Update and rename TheHurdleRace.py to easy/TheHurdleRace.py

--- easy/TheHurdleRace.py
+++ easy/TheHurdleRace.py
@@ -1,8 +1,8 @@
+# https://www.hackerrank.com/challenges/the-hurdle-race/problem
 # A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters 
-# have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by unit for
-# each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already 
-# clear all of the hurdles, return .
-
+# have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit
+# for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can 
+# already clear all of the hurdles, return 0.
 import math
 import os
 import random
@@ -14,6 +14,7 @@ def hurdleRace(k, height):
     if k >= max(height):
         return 0
     return max(height) - k
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
