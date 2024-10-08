8a3c84f0d0517c2cb9f157ec0bcf47cc7168f82b
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-08 17:18:43 +0300

    Update and rename Reduce Function to ReduceFunction.py

--- medium/ReduceFunction.py
+++ medium/ReduceFunction.py
@@ -1,12 +1,13 @@
-https://www.hackerrank.com/challenges/reduce-function/problem
-
-
+# https://www.hackerrank.com/challenges/reduce-function/problem
+# Given a list of rational numbers,find their product. Use reduce.
 from fractions import Fraction
 from functools import reduce
 
+
 def product(fracs):
-    t = Fraction(reduce(lambda x, y: x * y, fracs)) # complete this line with a reduce statement#
+    t = Fraction(reduce(lambda x, y: x * y, fracs))
     return t.numerator, t.denominator
+
 
 if __name__ == '__main__':
     fracs = []
