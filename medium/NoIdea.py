c1917508b3a837e5aa49f0a802398d279d7c39d1
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-08 17:06:33 +0300

    Update NoIdea.py

--- medium/NoIdea.py
+++ medium/NoIdea.py
@@ -1,8 +1,8 @@
-https://www.hackerrank.com/challenges/no-idea/problem
-There is an array of integers. There are also disjoint sets, A and B, each containing m integers. 
-You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0. 
-For each integer i in the array, if i in A you add 1 to your happiness. If i in B, you add -1
-to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
+# https://www.hackerrank.com/challenges/no-idea/problem
+# There is an array of integers. There are also disjoint sets, A and B, each containing m integers. 
+# You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0. 
+# For each integer i in the array, if i in A you add 1 to your happiness. If i in B, you add -1
+# to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
 
 
 def get_happiness(arr, set_a, set_b):
