0a3f6a1e03d83e1ef571e6d98465c02a7ef086a5
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-10-09 19:06:56 +0300

    Update ValidatingEmailAddressesWithAFilter.py

--- medium/ValidatingEmailAddressesWithAFilter.py
+++ medium/ValidatingEmailAddressesWithAFilter.py
@@ -1,9 +1,9 @@
-https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
-You are given an integer N followed by N email addresses. Your task is to print a list containing only valid email addresses in lexicographical order.
-
+# https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
+# You are given an integer N followed by N email addresses. Your task is to print a list 
+# containing only valid email addresses in lexicographical order.
 import re
 
-#the 'fun' function is the one I had to fill in for this problem
+
 def fun(s):
     pattern = r"^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
     return re.match(pattern, s) is not None
