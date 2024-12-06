34b6a366929cd4c87ffaa1f6cc5ce76a65d272b1
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-12-06 20:31:08 +0200

    Create MaximumPerimeterTriangle.py

---
+++ easy/MaximumPerimeterTriangle.py
@@ -0,0 +1,48 @@
+# https://www.hackerrank.com/challenges/maximum-perimeter-triangle/problem
+# Given an array of stick lengths, use 3 of them to construct a non-degenerate triangle
+# with the maximum possible perimeter. Return an array of the lengths of its sides as 3
+# integers in non-decreasing order.
+# If there are several valid triangles having the maximum perimeter:
+# Choose the one with the longest maximum side.
+# If more than one has that maximum, choose from them the one with the longest minimum
+# side.
+# If more than one has that maximum as well, print any one them.
+# If no non-degenerate triangle exists, return [-1].
+import math
+import os
+import random
+import re
+import sys
+
+
+def maximumPerimeterTriangle(sticks):
+    n = len(sticks)
+    sticks.sort()
+    valid_triangles = []
+    
+    for i in range(n - 2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if sticks[i] + sticks[j] > sticks[k]:
+                    valid_triangles.append([sticks[i], sticks[j], sticks[k]])
+    
+    
+    if valid_triangles:
+        return max(valid_triangles)
+    else:
+        return [-1]
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
